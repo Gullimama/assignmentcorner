66e57f61745519d95f725f939047520692db8d93
raju <33832428+Knightch@users.noreply.github.com>
2019-09-15 00:15:12 +0530

    backend changes

--- src/app/home/form/form.component.ts
+++ src/app/home/form/form.component.ts
@@ -18,39 +18,41 @@ import { CompileShallowModuleMetadata } from '@angular/compiler';
 export class FormComponent implements OnInit {
   Forms$: form[];
 
-  constructor(private formservice:FormService)  { 
+  constructor(private formservice: FormService) {
 
-    }
-    ngOnInit() {
-     // return this.formservice.getForms()
-      // .subscribe(data => this.Forms$=data);
-    }
-       onSubmit(){
-        var value=submit_by_id()
-        return value     
-      }  
-            
-  
-    }
-  const Http = new XMLHttpRequest();
-  const url='https://backend-dot-assignmentcorner.appspot.com/assignmentcorner/persistfile?fileData=change'
-  Http.open("GET",url);    
-  Http.send();
-
-Http.onreadystatechange=function(){
-  if(this.readyState==4 && this.status==200){
-    console.log(Http.responseText) 
   }
+  ngOnInit() {
+    // return this.formservice.getForms()
+    // .subscribe(data => this.Forms$=data);
+  }
+  onSubmit() {
+    var value = submit_by_id()
+    const Http = new XMLHttpRequest();
+    var url = 'https://backend-dot-assignmentcorner.appspot.com/assignmentcorner/persistfile?fileData='
+    url += value
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(Http.responseText)
+      }
+    }
+  }
+
+
 }
- function submit_by_id() { 
-  let title = document.getElementById("title").value
-  let date = document.getElementById("Deadline").value
-  let email = document.getElementById("Email").value
-  let number = document.getElementById("phone_number").value
-  let encode= [title,date,email,number];
-   for(var i=0;i<5;i++){
+
+function submit_by_id() {
+  let title = (document.getElementById("title") as HTMLInputElement).value
+  let project = (document.getElementById("Project_type") as HTMLInputElement).value
+  let date = (document.getElementById("Deadline") as HTMLInputElement).value
+  let email = (document.getElementById("Email") as HTMLInputElement).value
+  let number = (document.getElementById("phone_number") as HTMLInputElement).value
+  var finalString = title + ", " + project + ", " + date + ", " + email + ", " + number + "\n\r" 
+
     
-    var chanage = btoa(encode[i])  
-  }
-  console.log(title,date,email,number)  
+  finalString = btoa(finalString);
+  
+  return finalString;
 }
